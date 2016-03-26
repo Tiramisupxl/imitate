99316f0f81d5021c5b81acbf4bcafa1a90c08f3b
panxiaoli <463712331@qq.com>
2016-03-26 15:14:15 +0800

    粗略完成图片滚动，代码还需好好优化

--- from_Jean/main.js
+++ from_Jean/main.js
@@ -6,7 +6,6 @@ var artistPage=$("#artistPage");
 //页面一进来的鼠标滑动效果
 var hover=true;
 var gateHover=function(){
-    console.log(hover);
         developer.on("mouseover",function(){
             if(hover){
                 $(this).width("65%");
@@ -50,5 +49,63 @@ artistPage.on("click",function(){
         gateHover()
     }
 });
-
 gateHover();
+//左边图片滚动
+var n=0;
+var developerWidth=developer.find("li").width()*3+50;
+developer.find("ul").width(developerWidth);
+developer.on("mousewheel",function(){
+        e = window.event || e;
+    var delta = Math.max(-1, Math.min(1,
+        (e.wheelDelta || -e.deltaY || -e.detail)));
+    var width=$(".imgs").width();
+    var speed=100;
+    var radio=width/developerWidth;
+    //var scrollspeed=speed*radio;
+    console.log(developerWidth);
+    var scroll=true;
+    if(scroll) {
+        if (delta < 0) {
+            n+=1;
+        } else {
+            n--;
+        }
+     console.log(n);
+     developer.find("ul").css("transform","translateX(-"+speed*n+"px)");
+     developer.find(".scrollBar").css("transform","translateX("+speed*n+"px)");
+        if(n>15){
+            n=15
+        }
+        if(n<-1){
+            n=0
+        }
+    }
+});
+//右边图片滑动
+
+var artistWidth=artist.find("li").width()*3+50;
+artist.find("ul").width(artistWidth);
+artist.on("mousewheel",function(){
+    e = window.event || e;
+    var delta = Math.max(-1, Math.min(1,
+        (e.wheelDelta || -e.deltaY || -e.detail)));
+    var width=$(".imgs").width();
+    var speed=100;
+    var radio=width/developerWidth;
+    var scroll=true;
+    if(scroll) {
+        if (delta < 0) {
+            n+=1;
+        } else {
+            n--;
+        }
+        artist.find("ul").css("transform","translateX(-"+speed*n+"px)");
+        artist.find(".scrollBar").css("transform","translateX("+speed*n+"px)");
+        if(n>15){
+            n=15
+        }
+        if(n<-1){
+            n=0
+        }
+    }
+});
---
+++ jean.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Pxiaoli on 2016/3/25.
+ */
