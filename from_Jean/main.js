f3742b0c2a6e76209581a083b3696f4e36466e00
panxiaoli <463712331@qq.com>
2016-03-26 17:11:57 +0800

    初步完成，还需优化

--- from_Jean/main.js
+++ from_Jean/main.js
@@ -19,6 +19,8 @@ var gateHover=function(){
         });
         gate.addClass("init").removeClass("now-dev now-art");
         $(".imgs").hide();
+    $("ul").css("transform","translateX(0)");
+    $(".scrollBar").css("transform","translateX(0)");
 };
 //点击page
 var pageEnter=function(obj,nowClass){
@@ -50,62 +52,44 @@ artistPage.on("click",function(){
     }
 });
 gateHover();
-//左边图片滚动
-var n=0;
-var developerWidth=developer.find("li").width()*3+50;
-developer.find("ul").width(developerWidth);
-developer.on("mousewheel",function(){
-        e = window.event || e;
-    var delta = Math.max(-1, Math.min(1,
-        (e.wheelDelta || -e.deltaY || -e.detail)));
-    var width=$(".imgs").width();
-    var speed=100;
-    var radio=width/developerWidth;
-    //var scrollspeed=speed*radio;
-    console.log(developerWidth);
-    var scroll=true;
-    if(scroll) {
-        if (delta < 0) {
-            n+=1;
-        } else {
-            n--;
-        }
-     console.log(n);
-     developer.find("ul").css("transform","translateX(-"+speed*n+"px)");
-     developer.find(".scrollBar").css("transform","translateX("+speed*n+"px)");
-        if(n>15){
-            n=15
-        }
-        if(n<-1){
-            n=0
-        }
-    }
-});
-//右边图片滑动
 
-var artistWidth=artist.find("li").width()*3+50;
-artist.find("ul").width(artistWidth);
-artist.on("mousewheel",function(){
-    e = window.event || e;
-    var delta = Math.max(-1, Math.min(1,
-        (e.wheelDelta || -e.deltaY || -e.detail)));
-    var width=$(".imgs").width();
-    var speed=100;
-    var radio=width/developerWidth;
-    var scroll=true;
-    if(scroll) {
-        if (delta < 0) {
-            n+=1;
-        } else {
-            n--;
+//图片滚动
+var n=0;
+var imgScroll=function(obj){
+    var objWidth=obj.find("li").width()*3+50;
+    obj.find("ul").width(objWidth);
+    obj.on("mousewheel",function(){
+        var scroll;
+        if(gate.hasClass("now-dev")||gate.hasClass("now-art")) {
+            scroll=true
+        }else{
+            scroll=false
         }
-        artist.find("ul").css("transform","translateX(-"+speed*n+"px)");
-        artist.find(".scrollBar").css("transform","translateX("+speed*n+"px)");
-        if(n>15){
-            n=15
-        }
-        if(n<-1){
-            n=0
-        }
-    }
-});
+            e = window.event || e;
+            var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.deltaY || -e.detail)));
+            var width = obj.find(".imgs").width();
+            var speed = 100;
+            var endN = Math.floor((objWidth - width) / speed);
+            var scrollspeed = (width - 200) / endN;
+            console.log(scrollspeed);
+            if (scroll) {
+                if (delta < 0) {
+                    n += 1;
+                } else {
+                    n--;
+                }
+                if (n > endN) {
+                    n = endN
+                }
+                if (n < 0) {
+                    n = 0
+                }
+                obj.find("ul").css("transform", "translateX(-" + speed * n + "px)");
+                obj.find(".scrollBar").css("transform", "translateX(" + scrollspeed * n + "px)");
+
+                console.log(n);
+            }
+    });
+};
+imgScroll(developer);
+imgScroll(artist);
